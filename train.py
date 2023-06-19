6e857c01739cce10f06e3577c4535655e9191486
hash-brow <div4804@gmail.com>
2023-06-19 23:15:29 +0530

    add untested train.py

--- train.py
+++ train.py
@@ -2,6 +2,9 @@ import numpy as np
 import argparse
 import pathlib
 import sys
+import cv2
+from glob import glob
+import pandas as pd
 
 class denseLayer:
     def __init__(self, nInputs, nNeurons):
@@ -67,6 +70,8 @@ class meanSquare(loss):
     def forward(self, yPred, y):
         labels = len(yPred[0])
         yTrue = np.eye(labels)[y]
+
+
         return np.mean((yTrue - yPred) ** 2, axis = -1)
     
     def backward(self, dvalues, y):
@@ -88,26 +93,57 @@ def accuracy(yPred, y):
 # Optimization Algorithms
 
 class gd:
-    def __init__(self, learningRate = 1.0):
+    def __init__(self, learningRate):
         self.learningRate = learningRate
     
     def updateParams(self, layer):
         layer.weights += -self.learningRate * layer.dweights
         layer.biases += -self.learningRate * layer.dbiases
 
-class momentum:
-    pass
+class momentum():
+    def __init__(self, learningRate, momentum):
+        self.momentum = momentum
+        self.learningRate = learningRate
+
+    def updateParams(self, layer):
+        if not hasattr(layer, "weightMomentums"):
+            layer.weightMomentums = np.zeros_like(layer.weights)
+            layer.biasMomentums = np.zeros_like(layer.biases)
+        
+        weightUpdates = self.momentum * layer.weightMomentums - self.learningRate * layer.dweights
+        layer.weightMomentums = weightUpdates
+
+        biasUpdates = self.momentum * layer.biasMomentums - self.learningRate * layer.dweights
+        layer.biasMomentums = biasUpdates
+
+        layer.weights += weightUpdates
+        layer.biases += biasUpdates
 
 class nag:
-    pass
+    def __init__(self, learningRate, momentum):
+        self.learningRate = learningRate
+        self.momentum = momentum
+
+    def updateParams(self, layer):
+        if not hasattr(layer, "weightMomentums"):
+            layer.weightMomentums = np.zeros_like(layer.weights)
+            layer.biasMomentums = np.zeros_like(layer.biases)
+
+        weightLookAhead = layer.dweights - self.momentum * layer.weightMomentums
+        weightUpdates = self.momentum * layer.weightMomentums + self.learningRate * weightLookAhead
+        layer.weights -= weightUpdates
+
+        biasLookAhead = layer.dbiases - self.momentum * layer.biasMomentums
+        biasUpdates = self.momentum * layer.weightMomentums + self.learningRate * biasLookAhead
+        layer.biases -= biasUpdates
 
 class adam:
-    def __init__(self, learningRate = 0.01, decay = 0., epsilon = 1e-7, beta1 = 0.9, beta2 = 0.999):
+    def __init__(self, learningRate = 0.01, decay = 0.1, epsilon = 1e-7, momentum = 0.9, beta2 = 0.999):
         self.learningRate = learningRate
         self.decay = decay
         self.iterations = 0
         self.epsilon = epsilon
-        self.beta1 = beta1
+        self.beta1 = momentum
         self.beta2 = beta2
     
     def preUpdateParams(self):
@@ -139,8 +175,90 @@ class adam:
     def postUpdateParams(self):
         self.iterations += 1
 
-def main(learningRate, momentumVal, numHidden, sizes, activationFunction, lossFunction, optimizer, decay, train, test, saveDir, exptDir):
-    pass
+# Main function
+
+def main(learningRate, momentumVal, numHidden, sizes, activationFunction, lossFunction, optimizer, decay, train, test, saveDir, exptDir, opt):
+    nn = []
+    nn.append(denseLayer(32 * 32 * 3, sizes[0]))
+
+    for i in range(numHidden):
+        if i != numHidden - 1:
+            nn.append(denseLayer(sizes[i], sizes[i + 1]))
+        else:
+            nn.append(denseLayer(sizes[i], 1))
+
+    nn.append(denseLayer(1, 1))
+
+    nn = np.asarray(nn)
+
+    files = glob(f"{train}/*")
+
+    X = np.asarray([cv2.imread(x, cv2.IMREAD_UNCHANGED).flatten() for x in files])
+
+    y = pd.read_csv("trainLabels.csv")
+
+    encodingLabels = {
+        'frog': 0, 
+        'truck': 1, 
+        'deer': 2,
+        'automobile': 3,
+        'bird': 4,
+        'horse': 5,
+        'ship': 6,
+        'cat': 7,
+        'dog': 8,
+        'airplane': 9
+    }
+
+    reverseEncoding = ['frog', 'truck', 'deer', 'automobile', 'bird', 'horse', 'ship', 'cat', 'dog', 'airplane']
+
+    y = y['label'].to_numpy()
+
+    for i in range(y.shape[0]):
+        y[i] = encodingLabels[y[i]]
+
+    perm = np.random.permutation(len(y))
+    X, y = X[perm], y[perm]
+
+    for epoch in range(1, 6):
+        for step in range(1, len(X)//batchSize):
+            X_batch = X[(step - 1) * batchSize : step * batchSize]
+            y_batch = y[(step - 1) * batchSize : step * batchSize]
+
+            for i in range(len(nn)):                    
+                if i == 0:
+                    nn[i].forward(X_batch)
+                else:
+                    nn[i].forward(activationFunction.output)
+
+                activationFunction.forward(nn[i].output)
+
+            loss = lossFunction.forward(activationFunction.output, y)
+
+            predictions = np.argmax(lossFunction.output)
+
+            err = accuracy(predictions, y_batch)
+
+            if not step % 100:
+                print(f"Epoch {epoch}, Step {step}, Loss: {loss}, Error: {err}, lr: {activationFunction.lr}")
+
+            lossFunction.backward(lossFunction.output, y)
+            for i in range(len(nn) - 1, 0, -1):
+                if i == len(nn) - 1:
+                    nn[i].backward(lossFunction.dinputs)
+                else:
+                    nn[i].backward(activationFunction.dinputs)
+
+                activationFunction.backward(nn[i].dinputs)
+
+            if opt == "adam":
+                optimizer.preUpdateParams()
+            
+            optimizer.updateParams()
+
+            if opt == "adam":
+                optimizer.postUpdateParams()
+
 
 if __name__ == "__main__":
 
@@ -210,19 +328,30 @@ if __name__ == "__main__":
     else:
         lossFunction = crossEntropy()
 
+    if learningRate == None:
+        learningRate = 1.
+        print("learningRate not provided, defaulting to 1")
+
+    if momentumVal == None and (opt != "gd" or opt != "adam"):
+        momentumVal = 0.5
+        print("momentum not provided, defaulting to 0.5")
+    
     if opt == "gd":
         optimizer = gd(learningRate)
     elif opt == "momentum":
-        optimizer = momentum()
+        optimizer = momentum(learningRate, momentumVal)
     elif opt == "nag":
-        optimizer = nag()
+        optimizer = nag(learningRate, momentumVal)
     else:
-        optimizer = adam(learningRate)
+        optimizer = adam(learningRate, momentum=momentumVal)
 
     if anneal == "true":
         decay = .5
     else:
         decay = 1
 
-    main(learningRate, momentumVal, numHidden, sizes, activationFunction, lossFunction, optimizer, decay, train, test, saveDir, exptDir)
+    main(learningRate, momentumVal, numHidden, sizes, activationFunction, lossFunction, optimizer, decay, train, test, saveDir, exptDir, opt)
     
+
+# total steps per epoch = 50000 / batch_size
+# print every 100 steps
